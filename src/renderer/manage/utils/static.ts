fd9ea16909a55a6bdc69b5e07ca7f0ad3ab58b8a
萌萌哒赫萝 <ma_shiqing@163.com>
2023-03-17 18:32:59 +0800

    :construction: WIP: i18n of manage page, setting page done

--- src/renderer/manage/utils/static.ts
+++ src/renderer/manage/utils/static.ts
@@ -1,2 +1,3 @@
 export const cancelDownloadLoadingFileList = 'cancelDownloadLoadingFileList'
 export const refreshDownloadFileTransferList = 'refreshDownloadFileTransferList'
+export const selectDownloadFolder = 'selectDownloadFolder'
