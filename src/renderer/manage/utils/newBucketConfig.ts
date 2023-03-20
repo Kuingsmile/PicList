731847ac3f5c13ae2680eaae2f97a8be1ce0cab0
萌萌哒赫萝 <ma_shiqing@163.com>
2023-03-20 22:10:24 +0800

    :sparkles: Feature: i18n of new bucket config

--- src/renderer/manage/utils/newBucketConfig.ts
+++ src/renderer/manage/utils/newBucketConfig.ts
@@ -1,30 +1,31 @@
 import { AliyunAreaCodeName, QiniuAreaCodeName, TencentAreaCodeName } from '~/main/manage/utils/constants'
+import { T as $T } from '@/i18n'
 
 export const newBucketConfig:IStringKeyMap = {
   tcyun: {
-    name: '腾讯云',
+    name: $T('MANAGE_NEW_BUCKET_TCYUN_NAME'),
     icon: 'tcyun',
     configOptions: {
       BucketName: {
         required: true,
-        description: 'Bucket名称',
-        placeholder: '请输入Bucket名称',
+        description: $T('MANAGE_NEW_BUCKET_TCYUN_BUCKETNAME_DESC'),
+        placeholder: $T('MANAGE_NEW_BUCKET_TCYUN_BUCKETNAME_PLACEHOLDER'),
         paraType: 'string',
         component: 'input',
         default: 'piclist',
         rule: [
           {
             required: true,
-            message: 'Bucket名称不能为空',
+            message: $T('MANAGE_NEW_BUCKET_TCYUN_BUCKETNAME_RULE_MSG_A'),
             trigger: 'blur'
           },
           {
             validator: (rule: any, value: any, callback: any) => {
               const reg = /^[a-z0-9][a-z0-9-]{1,21}[a-z0-9]$/
               if (value.length > 23) {
-                callback(new Error('Bucket名称长度不能超过23个字符'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_TCYUN_BUCKETNAME_RULE_MSG_B')))
               } else if (!reg.test(value)) {
-                callback(new Error('Bucket名称只能包含小写字母、数字和中划线，且不能以中划线开头和结尾'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_TCYUN_BUCKETNAME_RULE_MSG_C')))
               } else {
                 callback()
               }
@@ -35,7 +36,7 @@ export const newBucketConfig:IStringKeyMap = {
       },
       region: {
         required: true,
-        description: '地域',
+        description: $T('MANAGE_NEW_BUCKET_TCYUN_REGION'),
         paraType: 'string',
         component: 'select',
         default: 'ap-nanjing',
@@ -43,43 +44,43 @@ export const newBucketConfig:IStringKeyMap = {
       },
       acl: {
         required: true,
-        description: '访问权限',
+        description: $T('MANAGE_NEW_BUCKET_TCYUN_ACL_DESC'),
         paraType: 'string',
         component: 'select',
         default: 'private',
         options: {
-          private: '私有',
-          publicRead: '公共读',
-          publicReadWrite: '公共读写'
+          private: $T('MANAGE_NEW_BUCKET_TCYUN_ACL_PRIVATE'),
+          publicRead: $T('MANAGE_NEW_BUCKET_TCYUN_ACL_PUBLIC_R'),
+          publicReadWrite: $T('MANAGE_NEW_BUCKET_TCYUN_ACL_PUBLIC_RW')
         }
       }
     },
     options: ['BucketName', 'region', 'acl']
   },
   aliyun: {
-    name: '阿里云',
+    name: $T('MANAGE_NEW_BUCKET_ALIYUN_NAME'),
     icon: 'aliyun',
     configOptions: {
       BucketName: {
         required: true,
-        description: 'Bucket名称',
-        placeholder: '请输入Bucket名称',
+        description: $T('MANAGE_NEW_BUCKET_ALIYUN_BUCKETNAME_DESC'),
+        placeholder: $T('MANAGE_NEW_BUCKET_ALIYUN_BUCKETNAME_PLACEHOLDER'),
         paraType: 'string',
         component: 'input',
         default: 'piclist',
         rule: [
           {
             required: true,
-            message: 'Bucket名称不能为空',
+            message: $T('MANAGE_NEW_BUCKET_ALIYUN_BUCKETNAME_RULE_MSG_A'),
             trigger: 'blur'
           },
           {
             validator: (rule: any, value: any, callback: any) => {
               const reg = /^[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$/
               if (value.length > 63) {
-                callback(new Error('Bucket名称长度不能超过63个字符'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_ALIYUN_BUCKETNAME_RULE_MSG_B')))
               } else if (!reg.test(value)) {
-                callback(new Error('Bucket名称只能包含小写字母、数字和中划线，且不能以中划线开头和结尾'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_ALIYUN_BUCKETNAME_RULE_MSG_C')))
               } else {
                 callback()
               }
@@ -90,7 +91,7 @@ export const newBucketConfig:IStringKeyMap = {
       },
       region: {
         required: true,
-        description: '地域',
+        description: $T('MANAGE_NEW_BUCKET_ALIYUN_REGION'),
         paraType: 'string',
         component: 'select',
         default: 'oss-cn-hangzhou',
@@ -98,43 +99,43 @@ export const newBucketConfig:IStringKeyMap = {
       },
       acl: {
         required: true,
-        description: '访问权限',
+        description: $T('MANAGE_NEW_BUCKET_ALIYUN_ACL_DESC'),
         paraType: 'string',
         component: 'select',
         default: 'private',
         options: {
-          private: '私有',
-          publicRead: '公共读',
-          publicReadWrite: '公共读写'
+          private: $T('MANAGE_NEW_BUCKET_ALIYUN_ACL_PRIVATE'),
+          publicRead: $T('MANAGE_NEW_BUCKET_ALIYUN_ACL_PUBLIC_R'),
+          publicReadWrite: $T('MANAGE_NEW_BUCKET_ALIYUN_ACL_PUBLIC_RW')
         }
       }
     },
     options: ['BucketName', 'region', 'acl']
   },
   qiniu: {
-    name: '七牛云',
+    name: $T('MANAGE_NEW_BUCKET_QINIU_NAME'),
     icon: 'qiniu',
     configOptions: {
       BucketName: {
         required: true,
-        description: 'Bucket名称',
-        placeholder: '请输入Bucket名称',
+        description: $T('MANAGE_NEW_BUCKET_QINIU_BUCKETNAME_DESC'),
+        placeholder: $T('MANAGE_NEW_BUCKET_QINIU_BUCKETNAME_PLACEHOLDER'),
         paraType: 'string',
         component: 'input',
         default: 'piclist',
         rule: [
           {
             required: true,
-            message: 'Bucket名称不能为空',
+            message: $T('MANAGE_NEW_BUCKET_QINIU_BUCKETNAME_RULE_MSG_A'),
             trigger: 'blur'
           },
           {
             validator: (rule: any, value: any, callback: any) => {
               const reg = /^[a-z0-9][a-z0-9-]{1,61}[a-z0-9]$/
               if (value.length > 63) {
-                callback(new Error('Bucket名称长度不能超过63个字符'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_QINIU_BUCKETNAME_RULE_MSG_B')))
               } else if (!reg.test(value)) {
-                callback(new Error('Bucket名称只能包含小写字母、数字和中划线，且不能以中划线开头和结尾'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_QINIU_BUCKETNAME_RULE_MSG_C')))
               } else {
                 callback()
               }
@@ -145,7 +146,7 @@ export const newBucketConfig:IStringKeyMap = {
       },
       region: {
         required: true,
-        description: '地域',
+        description: $T('MANAGE_NEW_BUCKET_QINIU_REGION'),
         paraType: 'string',
         component: 'select',
         default: 'z0',
@@ -153,7 +154,7 @@ export const newBucketConfig:IStringKeyMap = {
       },
       acl: {
         required: true,
-        description: '公开访问',
+        description: $T('MANAGE_NEW_BUCKET_QINIU_ACL_DESC'),
         paraType: 'boolean',
         component: 'switch',
         default: false
@@ -162,29 +163,29 @@ export const newBucketConfig:IStringKeyMap = {
     options: ['BucketName', 'region', 'acl']
   },
   upyun: {
-    name: '又拍云',
+    name: $T('MANAGE_NEW_BUCKET_UPYUN_NAME'),
     icon: 'upyun',
     configOptions: {
       BucketName: {
         required: true,
-        description: 'Bucket名称',
-        placeholder: '请输入Bucket名称',
+        description: $T('MANAGE_NEW_BUCKET_UPYUN_BUCKETNAME_DESC'),
+        placeholder: $T('MANAGE_NEW_BUCKET_UPYUN_BUCKETNAME_PLACEHOLDER'),
         paraType: 'string',
         component: 'input',
         default: 'piclist',
         rule: [
           {
             required: true,
-            message: 'Bucket名称不能为空',
+            message: $T('MANAGE_NEW_BUCKET_UPYUN_BUCKETNAME_RULE_MSG_A'),
             trigger: 'blur'
           },
           {
             validator: (rule: any, value: any, callback: any) => {
               const reg = /^[a-z][a-z0-9-]{4,19}$/
               if (value.length > 23 || value.length < 5) {
-                callback(new Error('Bucket名称长度为5-20个字符'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_UPYUN_BUCKETNAME_RULE_MSG_B')))
               } else if (!reg.test(value)) {
-                callback(new Error('Bucket名称只能包含小写字母、数字和中划线，且不能以中划线开头和结尾'))
+                callback(new Error($T('MANAGE_NEW_BUCKET_UPYUN_BUCKETNAME_RULE_MSG_C')))
               } else {
                 callback()
               }
@@ -195,28 +196,28 @@ export const newBucketConfig:IStringKeyMap = {
       },
       operator: {
         required: true,
-        description: '操作员',
-        placeholder: '请输入操作员',
+        description: $T('MANAGE_NEW_BUCKET_UPYUN_OPERATORNAME_DESC'),
+        placeholder: $T('MANAGE_NEW_BUCKET_UPYUN_OPERATORNAME_PLACEHOLDER'),
         paraType: 'string',
         component: 'input',
         rule: [
           {
             required: true,
-            message: '操作员不能为空',
+            message: $T('MANAGE_NEW_BUCKET_UPYUN_OPERATORNAME_RULE_MSG_A'),
             trigger: 'blur'
           }
         ]
       },
       password: {
         required: true,
-        description: '密码',
-        placeholder: '请输入密码',
+        description: $T('MANAGE_NEW_BUCKET_UPYUN_PASSWORD_DESC'),
+        placeholder: $T('MANAGE_NEW_BUCKET_UPYUN_PASSWORD_PLACEHOLDER'),
         paraType: 'string',
         component: 'input',
         rule: [
           {
             required: true,
-            message: '密码不能为空',
+            message: $T('MANAGE_NEW_BUCKET_UPYUN_PASSWORD_RULE_MSG_A'),
             trigger: 'blur'
           }
         ]
