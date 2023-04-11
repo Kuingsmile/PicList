99155264ad9f6b2d5fc58e4669fb8d11c2cfc627
萌萌哒赫萝 <ma_shiqing@163.com>
2023-04-11 17:07:22 +0800

    :package: Chore: modify link generate script

--- scripts/link.js
+++ scripts/link.js
@@ -5,20 +5,21 @@ const generateURL = (platform, ext, prefix = 'PicList-') => {
   return `https://release.piclist.cn/latest/${prefix}${version}${platform}${ext}`
 }
 
-const platformExtList = [
-  ['-arm64', '.dmg', 'PicList-'],
-  ['-x64', '.dmg', 'PicList-'],
-  ['-universal', '.dmg', 'PicList-'],
-  ['', '.AppImage', 'PicList-'],
-  ['-ia32', '.exe', 'PicList-Setup-'],
-  ['-x64', '.exe', 'PicList-Setup-'],
-  ['', '.exe', 'PicList-Setup-'],
-  ['_amd64', '.snap', 'piclist_']
-]
+const template = `
+### 加速下载地址
 
-const links = platformExtList.map(([arch, ext, prefix]) => {
-  const markdownLink = `[${prefix}${version}${arch}${ext}](${generateURL(arch, ext, prefix)})`
-  return markdownLink
-})
+####MacOS
+[PicList-${version}-arm64.dmg](${generateURL('-arm64', '.dmg', 'PicList-')})
+[PicList-${version}-x64.dmg](${generateURL('-x64', '.dmg', 'PicList-')})
+[PicList-${version}-universal.dmg](${generateURL('-universal', '.dmg', 'PicList-')})
 
-console.log(links.join('\n'))
+####Windows
+[PicList-Setup-${version}-ia32.exe](${generateURL('-ia32', '.exe', 'PicList-Setup-')})
+[PicList-Setup-${version}-x64.exe](${generateURL('-x64', '.exe', 'PicList-Setup-')})
+[PicList-Setup-${version}.exe](${generateURL('', '.exe', 'PicList-Setup-')})
+
+####Linux
+[PicList-${version}.AppImage](${generateURL('', '.AppImage', 'PicList-')})
+[piclist_${version}_amd64.snap](${generateURL('_amd64', '.snap', 'piclist_')})`
+
+console.log(template)
