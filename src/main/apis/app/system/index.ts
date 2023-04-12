9305293a95d70bed22dc35fdaa2225e9fa160feb
萌萌哒赫萝 <ma_shiqing@163.com>
2023-04-12 15:37:42 +0800

    :bug: Fix: mac shortcut bug

--- src/main/apis/app/system/index.ts
+++ src/main/apis/app/system/index.ts
@@ -100,6 +100,18 @@ export function createMenu () {
       submenu
     },
     {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A' }
+      ]
+    },
+    {
       label: T('QUIT'),
       submenu: [
         {
