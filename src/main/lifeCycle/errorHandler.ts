dc1458ffefd45c7abc15fae2e1f4f2b503922e5d
萌萌哒赫萝 <ma_shiqing@163.com>
2023-04-15 22:15:20 +0800

    :bug: Fix: sync with picgo, suppress epiep error

--- src/main/lifeCycle/errorHandler.ts
+++ src/main/lifeCycle/errorHandler.ts
@@ -9,7 +9,7 @@ const logger = getLogger(LOG_PATH, 'PicList')
 // since the error may occur in picgo-core
 // so we can't use the log from picgo
 
-const handleProcessError = (error: Error) => {
+const handleProcessError = (error: Error | string) => {
   logger('error', error)
 }
 
@@ -20,3 +20,21 @@ process.on('uncaughtException', error => {
 process.on('unhandledRejection', (error: any) => {
   handleProcessError(error)
 })
+
+// acconrding to https://github.com/Molunerfinn/PicGo/commit/7363be798cfef11e980934e542817ff1d6c04389#diff-896d0db4fbd446798fbffec14d456b4cd98d4c72c46856c770a585fa7ab0926f
+function bootstrapEPIPESuppression () {
+  let suppressing = false
+  function logEPIPEErrorOnce () {
+    if (suppressing) {
+      return
+    }
+
+    suppressing = true
+    handleProcessError('Detected EPIPE error; suppressing further EPIPE errors')
+  }
+
+  require('epipebomb')(process.stdout, logEPIPEErrorOnce)
+  require('epipebomb')(process.stderr, logEPIPEErrorOnce)
+}
+
+bootstrapEPIPESuppression()
