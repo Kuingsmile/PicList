5018ce7ce7f831499eac7e830656b01ddaee97ca
萌萌哒赫萝 <ma_shiqing@163.com>
2023-07-07 01:21:16 -0700

    :sparkles: Feature: add a new buildin picbed - local pc

--- src/universal/utils/getLatestVersion.ts
+++ src/universal/utils/getLatestVersion.ts
@@ -7,13 +7,13 @@ export const getLatestVersion = async (): Promise<string> => {
     const { data: normalList } = await axios.get(RELEASE_URL)
     return normalList[0].name
   } catch (err) {
-    console.log(err)
+    console.error('Error fetching latest version: ', err)
     try {
       const { data } = await axios.get(`${RELEASE_URL_BACKUP}/latest.yml`)
       const r = yaml.load(data) as IStringKeyMap
       return r.version
     } catch (err) {
-      console.log(err)
+      console.error('Error fetching backup latest version: ', err)
       return ''
     }
   }
