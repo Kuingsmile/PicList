ce8bf3d810c50fcd278eaf03d6037088139dbc6c
Kuingsmile <96409857+Kuingsmile@users.noreply.github.com>
2026-09-21 02:43:17 -0700

    :bug: Fix(server): bound port retries and serialize lifecycle operations (#568) (#586)
    close #568

--- src/main/fileServer/index.ts
+++ src/main/fileServer/index.ts
@@ -5,39 +5,50 @@ import picgo from '@core/picgo'
 import logger from '@core/picgo/logger'
 import fs from 'fs-extra'
 
+import { closeServer, listenOnce } from '~/utils/serverLifecycle'
+
 export const imgFilePath = path.join(picgo.baseDir, 'imgTemp')
 fs.ensureDirSync(imgFilePath)
 
 const serverPort = 36699
 
-let server: http.Server
+let server: http.Server | undefined
+let operation: Promise<void> = Promise.resolve()
 
 export function startFileServer() {
-  server = http.createServer((req, res) => {
-    const requestPath = req.url?.split('?')[0]
-    const filePath = path.join(imgFilePath, decodeURIComponent(requestPath as string))
+  operation = operation.then(async () => {
+    if (server?.listening) return
+    server = http.createServer((req, res) => {
+      const requestPath = req.url?.split('?')[0]
+      const filePath = path.join(imgFilePath, decodeURIComponent(requestPath as string))
 
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(404)
-        res.end('404 Not Found')
-      } else {
-        res.end(data)
-      }
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(404)
+          res.end('404 Not Found')
+        } else {
+          res.end(data)
+        }
+      })
     })
-  })
-
-  server
-    .listen(serverPort, () => {
+    try {
+      await listenOnce(server, serverPort)
       logger.info(`File server is running, http://localhost:${serverPort}`)
-    })
-    .on('error', err => {
-      logger.error(err)
-    })
+    } catch (error) {
+      await closeServer(server)
+      server = undefined
+      logger.error(`File server could not start on port ${serverPort}`, error as Error)
+    }
+  })
+  return operation
 }
 
 export function stopFileServer() {
-  server.close(() => {
+  operation = operation.then(async () => {
+    if (!server) return
+    await closeServer(server)
+    server = undefined
     logger.info('File server is stopped')
   })
+  return operation
 }
